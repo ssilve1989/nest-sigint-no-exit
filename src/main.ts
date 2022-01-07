3dd6bc0dd98bd714d0a939eef87675392e66ef7c
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-01-07 13:42:03 -0400

    add disabled usage with fastify

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { Logger } from '@nestjs/common';
 import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const logger = new Logger();
+  // const app = await NestFactory.create(AppModule, new FastifyAdapter());
   const app = await NestFactory.create(AppModule);
 
+  const logger = new Logger('bootstrap');
   app.use(
     morgan(process.env.NODE_ENV === 'production' ? 'tiny' : 'dev', {
       stream: {
