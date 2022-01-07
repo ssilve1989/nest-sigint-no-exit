cf90f4d7c1e51b4ee264752a42bf49ddf5ecb6b3
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-01-07 13:30:11 -0400

    drop useless files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module, OnApplicationShutdown } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements OnApplicationShutdown {
   onApplicationShutdown() {
-    console.log('shutting down');
+    console.log('>>>> shutting down');
   }
 }
